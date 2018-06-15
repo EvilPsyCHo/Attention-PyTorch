5603f87f3abf28a50348e65184058b42e570ef86
evilpsycho <evilpsycho42@gmail.com>
2018-06-15 18:41:48 +0800

    upodate

--- models/hierarchical_attention.py
+++ models/hierarchical_attention.py
@@ -20,20 +20,40 @@ class HAN(nn.Module):
                           bidirectional=self.args["bi"])
         self.bi = 2 if self.args["bi"] else 1
         self.att = nn.Linear(self.args["hidden_size"] * self.bi, self.args["att_size"])
-        self.uw = nn.Parameter(torch.Tensor(self.args["att_size"], 1))
-        self.uw.data.uniform_(-0.1, 0.1)
-        self.fc = nn.Linear(self.args[""], self.args["class_num"])
+        self.lookup_B = nn.Embedding(self.args["vocab_size"],
+                                     self.args["att_size"])
+        self.fc = nn.Linear(self.args["hidden_size"] * self.bi, self.args["class_num"])
+        self.max_length = self.args["max_length"]
 
     def forward(self, x):
         x_embed = self.lookup(x)  # x_embed (batch, seq, embed)
+        x_embed_b = self.lookup_B(x)
         out, _ = self.gru(x_embed)  # out (batch, seq, hidden_size * bi)
-        att_q = self.att(x)  # att_q (batch, seq, att_size)
-        torch.bmm
+        att_q = self.att(out)  # att_q (batch, seq, att_size)
+        att = []
+        for t in range(self.max_length):
+            print(att_q[:, t, :].size())
+            att.append(torch.bmm(att_q[:, t, :], x_embed_b[:, t, :]))
+        return att
 
 
 
 
+if __name__ == "__main__":
+    p = {
+        "vocab_size": 1000,
+        "class_num": 10,
+        "bi": True,
+        "hidden_size": 20,
+        "embed_dim": 60,
+        "att_size": 10,
+        "dropout": 0.1,
+        "max_length": 10,
+    }
+    import numpy as np
+    x = np.random.randint(1, 1000, 100).reshape(-1, 10)
+    x = torch.tensor(x)
+    model = HAN(p)
+    model(x).size()
 
-# gru = nn.GRU(60, 20, bidirectional=True, batch_first=True)
-# x = torch.randn([8, 10, 60])
-# output, h = gru(x)
+    att = ScaledDotProductAttention(10)
